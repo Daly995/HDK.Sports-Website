5c7108f80fcff7f53dc95f291a3f6b3afa6adc6b
Ryne Dalrymple <10953035@uvu.edu>
2023-08-18 21:43:36 -0600

    Added front-end JavaScript.

---
+++ main.js
@@ -0,0 +1,5 @@
+// main.js
+document.addEventListener("DOMContentLoaded", () => {
+    // Front-end logic for adding items to the cart and updating UI
+    // This might involve event listeners, DOM manipulation, etc.
+});
